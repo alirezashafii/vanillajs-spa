87356b918e0120bd1b5e5be69d7950437db7bc30
Alireza Shafi'i <16956112+alirezashafii@users.noreply.github.com>
2022-08-04 17:52:35 +0430

    about component created

---
+++ javascript/about.js
@@ -0,0 +1,53 @@
+const About = () => {
+  return `<div class="about-section">
+            <p>
+            In the 19th century the rivalry between Russia and Great Britain 
+            in Iran took the form of economic intervention. After losing several battles 
+            with Russia, Iran was forced to grant economic and naval benefits to that 
+            country in Gīlān, under the treaties of 1813 and 1828. In 1907 Great Britain 
+            and Russia divided Iran into three zones; the northernmost, including 
+            the province of Gīlān, was the Russian zone. These events led to 
+            the growth of nationalist movements, and contingents recruited from Tabrīz, 
+            Gīlān, and Esfahān liberated Tehrān, with the result that Ahmad Mīrzā, 
+            son of Shāh Mohammad Alī, was proclaimed ruler. In the aftermath 
+            of the Russian Revolution, Bolshevik troops had control 
+            of almost the entire Iranian Caspian seacoast, and 
+            a separatist group proclaimed the short-lived Soviet Socialist Republic of Gīlān. 
+            In the treaty of 1921 the Soviet Union handed back the region around Enzeli 
+            and gave Iran equal navigation rights on the Caspian Sea.
+            </p>
+            <p>
+            Gīlān divides into a coastal plain including the large delta of the Safīd Rūd 
+            and adjacent parts of the Elburz Mountains. The soil is fertile loam, with dunes 
+            and marshy stretches along the lower plain. The jungle-like forest contains partly 
+            endemic species such as the Caucasian wingnut (a kind of walnut), and silk trees 
+            cover part of the plain. Animals include wild boar, lynx, panther, hyena, jackal, 
+            and deer, with waterfowl in the coastal stretches. On most cultivated land in Gīlān, 
+            rice is grown. Other crops include tobacco, fruits, vegetables, and tea (grown in 
+            the foothills above the rice fields). Fishing, developed by the Russians in the 19th century, 
+            has been government run since 1953 and is important; most of the catch 
+            (sturgeon, salmon, whitefish) is either dried or canned and is exported, 
+            as is the caviar, which in the early 1970s accounted for about one-fifth 
+            of world production. Gīlān has a few modern factories, mainly for tea and rice processing. 
+            Modern developments include a dam at Manjīl.
+            </p>
+            <p>
+            Gīlān’s capital and commercial centre is Rasht, which has a silk mill and a plant processing 
+            kenaf (hemp) fibre; rice-straw mats are also made. Rasht is linked to Qazvīn, Tehrān, 
+            and other coastal ports by road; it has an airport, and a natural-gas pipeline passes through 
+            the city en route to Azerbaijan. Mahījān, Langarūd, and Bandar-e Anzalī (a busy Caspian port) 
+            are other important economic centres. Area 5,679 square miles (14,709 square km). Pop. (2006) 2,404,861.
+            </p>
+            <p>
+            It's about 15 miles (24 km) south of the Caspian Sea on a branch of the Sefīd River, 
+            where the higher ground merges into the marshlands fringing the Mordāb, or Anzalī (formerly Pahlavī), 
+            lagoon. Rasht’s importance as the main city of the Gīlān region dates from Russia’s southward expansion 
+            in the 17th century. The city suffered severely during World Wars I and II from Russian occupation and 
+            afterward from economic decline. The city is surrounded by rice fields and areas of half-cleared jungle. 
+            The mostly two-storied houses have much woodwork, such as broad verandas and overhanging eaves; roofs are 
+            red-tiled, and the houses often are raised from the ground. Modernization is confined to the main streets.
+            </p>
+        </div>`;
+};
+
+export default About;
--- javascript/main.js
+++ javascript/main.js
@@ -1,25 +1,25 @@
 // GSAP
-const tl = gsap.timeline({
-  defaults: { duration: 0.75, ease: "power3.out" },
-});
+// const tl = gsap.timeline({
+//   defaults: { duration: 0.75, ease: "power3.out" },
+// });
 
-tl.fromTo(
-  ".hero-img",
-  { scale: 0.9, borderRadius: "0rem" },
-  {
-    scale: 1,
-    borderRadius: "2rem",
-    duration: 2.5,
-    ease: "elastic.out(1.5, 1",
-  }
-);
+// tl.fromTo(
+//   ".hero-img",
+//   { scale: 0.9, borderRadius: "0rem" },
+//   {
+//     scale: 1,
+//     borderRadius: "2rem",
+//     duration: 2.5,
+//     ease: "elastic.out(1.5, 1",
+//   }
+// );
 
-tl.fromTo(".cta1", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
-tl.fromTo(".cta3", { x: "-100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
-tl.fromTo(".cta2", { y: "-100%", opacity: 0.5 }, { y: 0, opacity: 1 }, "<20%");
-tl.fromTo(".cta4", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
-tl.fromTo(".cta6", { x: "-100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
-tl.fromTo(".cta5", { y: "100%", opacity: 0.5 }, { y: 0, opacity: 1 }, "<20%");
+// tl.fromTo(".cta1", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
+// tl.fromTo(".cta3", { x: "-100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
+// tl.fromTo(".cta2", { y: "-100%", opacity: 0.5 }, { y: 0, opacity: 1 }, "<20%");
+// tl.fromTo(".cta4", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
+// tl.fromTo(".cta6", { x: "-100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
+// tl.fromTo(".cta5", { y: "100%", opacity: 0.5 }, { y: 0, opacity: 1 }, "<20%");
 
 const logo = document.querySelector("h1");
 const letters = logo.textContent.split("");
@@ -63,38 +63,38 @@ logo.addEventListener("click", () => {
   }
 });
 
-// Horizontal Scrolling Section
-const scroll = document.querySelector(".scroll");
-let isDown = false;
-let scrollX;
-let scrollLeft;
+// // Horizontal Scrolling Section
+// const scroll = document.querySelector(".scroll");
+// let isDown = false;
+// let scrollX;
+// let scrollLeft;
 
-// Mouse Up Function
-scroll.addEventListener("mouseup", () => {
-  isDown = false;
-  scroll.classList.remove("active");
-});
+// // Mouse Up Function
+// scroll.addEventListener("mouseup", () => {
+//   isDown = false;
+//   scroll.classList.remove("active");
+// });
 
-// Mouse Leave Function
-scroll.addEventListener("mouseleave", () => {
-  isDown = false;
-  scroll.classList.remove("active");
-});
+// // Mouse Leave Function
+// scroll.addEventListener("mouseleave", () => {
+//   isDown = false;
+//   scroll.classList.remove("active");
+// });
 
-// Mouse Down Function
-scroll.addEventListener("mousedown", (e) => {
-  e.preventDefault();
-  isDown = true;
-  scroll.classList.add("active");
-  scrollX = e.pageX - scroll.offsetLeft;
-  scrollLeft = scroll.scrollLeft;
-});
+// // Mouse Down Function
+// scroll.addEventListener("mousedown", (e) => {
+//   e.preventDefault();
+//   isDown = true;
+//   scroll.classList.add("active");
+//   scrollX = e.pageX - scroll.offsetLeft;
+//   scrollLeft = scroll.scrollLeft;
+// });
 
-// Mouse Move Function
-scroll.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  let element = e.pageX - scroll.offsetLeft;
-  let scrolling = (element - scrollX) * 2;
-  scroll.scrollLeft = scrollLeft - scrolling;
-});
+// // Mouse Move Function
+// scroll.addEventListener("mousemove", (e) => {
+//   if (!isDown) return;
+//   e.preventDefault();
+//   let element = e.pageX - scroll.offsetLeft;
+//   let scrolling = (element - scrollX) * 2;
+//   scroll.scrollLeft = scrollLeft - scrolling;
+// });
